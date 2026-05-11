7535b95306f8bb373955113200c21e4c2edfc01a
jaekpavia <jakepavia86@gmail.com>
2026-05-11 17:25:51 +0800

    dashboard css unfinished

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,35 @@
 function logout() {
     window.location.href = "login.html"; // back to login page
 }
+
+function goToRooms() {
+    window.location.href = "dashboard.html";
+}
+
+function goToCheckIn() {
+    window.location.href = "check-in.html";
+}
+
+function goToCheckOut() {
+    window.location.href = "check-out.html";
+}
+
+function goToServices() {
+    window.location.href = "services.html";
+}
+
+function goToInventory() {
+    window.location.href = "inventory.html";
+}
+
+function goToTransactionHistory() {
+    window.location.href = "transaction-history.html";
+}
+
+function goToGuestsInformation() {
+    window.location.href = "guests-information.html";
+}
+
+function goToEmployeesAndAccounts() {
+    window.location.href = "employees-and-accounts.html";
+}
